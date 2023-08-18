dc0547977bbce65b2fe5f6a215b6533c913ad168
Diogo Da Silva Souza <diogoslvsouza04@gmail.com>
2023-08-18 08:59:36 -0300

    feat: menu & theme functionalities

--- js/index.js
+++ js/index.js
@@ -7,23 +7,75 @@ function toggleClasses(element, classNames) {
 }
 
 function mobileMenu() {
+  const header = document.querySelector("header");
   const menuBtn = document.querySelector("#menuMobile");
   const menuIcon = menuBtn.querySelector("i");
+  const themeBtns = document.querySelector(".btnIcon");
+  const sidePanel = document.querySelector(".header__categories--sidepanel");
 
-  menuBtn.addEventListener("click", () => {
+  const areasToExclude = [header, themeBtns, sidePanel];
+
+  menuBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    if (!menuIcon.classList.contains("fa-xmark")) {
+      sidePanel.style.width = "100%";
+    } else {
+      sidePanel.style.width = "0";
+    }
     toggleClasses(menuIcon, ["fa-xmark"]);
+  });
+
+  window.addEventListener("click", (e) => {
+    const isClickInsideExcludedArea = areasToExclude.some((area) =>
+      area.contains(e.target)
+    );
+
+    if (!isClickInsideExcludedArea && menuIcon.classList.contains("fa-xmark")) {
+      sidePanel.style.width = "0";
+      toggleClasses(menuIcon, ["fa-xmark"]);
+    }
   });
 }
 
 function themeMode() {
   const html = document.querySelector("html");
-  const themeBtn = document.querySelector("#themeBtn");
-  const themeIcon = themeBtn.querySelector("i");
+  const themeBtns = document.querySelectorAll(".btnIcon");
 
-  const darkTheme = localStorage.getItem("@weartake: dark");
+  themeBtns.forEach((btn) => {
+    const themeIcon = btn.querySelector("i");
+    btn.addEventListener("click", () => {
+      toggleClasses(themeIcon, [
+        "fa-moon",
+        "fa-fade",
+        "fa-sun",
+        "fa-spin-pulse",
+      ]);
+      toggleClasses(html, ["dark"]);
+      const isDarkMode =
+        html.classList.contains("dark") &&
+        themeIcon.classList.contains("fa-sun");
 
-  themeBtn.addEventListener("click", () => {
-    toggleClasses(themeIcon, ["fa-moon", "fa-fade", "fa-sun", "fa-spin-pulse"]);
+      localStorage.setItem(
+        "@weartake:theme-mode",
+        JSON.stringify({ mode: isDarkMode })
+      );
+    });
+
+    const savedTheme = localStorage.getItem("@weartake:theme-mode");
+    if (savedTheme) {
+      const parsedTheme = JSON.parse(savedTheme);
+      if (parsedTheme.mode) {
+        html.classList.add("dark");
+        if (!themeIcon.classList.contains("fa-sun")) {
+          toggleClasses(themeIcon, [
+            "fa-moon",
+            "fa-fade",
+            "fa-sun",
+            "fa-spin-pulse",
+          ]);
+        }
+      }
+    }
   });
 }
 
