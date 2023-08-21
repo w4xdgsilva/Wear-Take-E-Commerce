888380cc57d98f5554abc698b08f4193a5419db1
Diogo Da Silva Souza <diogoslvsouza04@gmail.com>
2023-08-21 16:33:35 -0300

    feat7fixes: code improvement

--- js/index.js
+++ js/index.js
@@ -15,26 +15,53 @@ function mobileMenu() {
 
   const areasToExclude = [header, themeBtns, sidePanel];
 
-  menuBtn.addEventListener("click", (e) => {
-    e.stopPropagation();
-    if (!menuIcon.classList.contains("fa-xmark")) {
-      sidePanel.style.width = "100%";
-    } else {
-      sidePanel.style.width = "0";
-    }
-    toggleClasses(menuIcon, ["fa-xmark"]);
-  });
+  function openSidePanel() {
+    sidePanel.style.width = "100%";
+    menuIcon.classList.add("fa-xmark");
+  }
 
-  window.addEventListener("click", (e) => {
+  function closeSidePanel() {
+    sidePanel.style.width = "0";
+    menuIcon.classList.remove("fa-xmark");
+  }
+
+  function toggleMenu() {
+    if (!menuIcon.classList.contains("fa-xmark")) {
+      openSidePanel();
+    } else {
+      closeSidePanel();
+    }
+  }
+
+  function handleMenuBtnClick(e) {
+    e.stopPropagation();
+    toggleMenu();
+  }
+
+  function handleWindowResize() {
+    if (window.innerWidth > 769) {
+      sidePanel.style.width = "0";
+      menuIcon.classList.remove("fa-xmark");
+    }
+  }
+
+  function handleWindowClick(e) {
     const isClickInsideExcludedArea = areasToExclude.some((area) =>
       area.contains(e.target)
     );
 
     if (!isClickInsideExcludedArea && menuIcon.classList.contains("fa-xmark")) {
-      sidePanel.style.width = "0";
-      toggleClasses(menuIcon, ["fa-xmark"]);
+      if (sidePanel.style.width === "0px") {
+        openSidePanel();
+      } else {
+        closeSidePanel();
+      }
     }
-  });
+  }
+
+  menuBtn.addEventListener("click", handleMenuBtnClick);
+  window.addEventListener("resize", handleWindowResize);
+  window.addEventListener("click", handleWindowClick);
 }
 
 function themeMode() {
@@ -51,6 +78,7 @@ function themeMode() {
         "fa-spin-pulse",
       ]);
       toggleClasses(html, ["dark"]);
+      html.style.transition = "background-color ease-in-out 1s";
       const isDarkMode =
         html.classList.contains("dark") &&
         themeIcon.classList.contains("fa-sun");
