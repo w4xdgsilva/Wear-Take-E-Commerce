7ec2fdaa26480155bd32fdac03109680ea2c24b6
Diogo Da Silva Souza <diogoslvsouza04@gmail.com>
2023-09-01 16:22:13 -0300

    fix: database img fixes

--- js/database.js
+++ js/database.js
@@ -1,7 +1,7 @@
 const data = [
   {
     id: 1,
-    img: "../assets/img/jaqueta.svg",
+    img: "assets/img/jaqueta.svg",
     nameItem: "Jacket",
     description:
       "Energize seu Guarda-Roupa de Inverno com uma Explosão de Cores! Dê um toque vibrante ao seu estilo de inverno com esta jaqueta radiante que vai iluminar até os dias mais frios.",
@@ -11,7 +11,7 @@ const data = [
   },
   {
     id: 2,
-    img: "../assets/img/gorro.svg",
+    img: "assets/img/gorro.svg",
     nameItem: "Black Hat",
     description:
       "Gorro Next.js: Estilo e Conforto em Um Só Produto! O gorro Next.js combina beleza bordada com ajuste confortável, garantindo estilo e aquecimento ao longo do ano.",
@@ -21,7 +21,7 @@ const data = [
   },
   {
     id: 3,
-    img: "../assets/img/mascara.svg",
+    img: "assets/img/mascara.svg",
     nameItem: "Mask",
     description:
       "Máscara Facial Durável: Proteção com Elegância! Nossa máscara facial durável possui duas camadas tratadas, presilhas ajustáveis e combina estilo com segurança para o uso diário.",
@@ -31,7 +31,7 @@ const data = [
   },
   {
     id: 4,
-    img: "../assets/img/camiseta_preta.svg",
+    img: "assets/img/camiseta_preta.svg",
     nameItem: "Black T-Shirt",
     description:
       "T-Shirt Essencial: Caimento Perfeito com Toque Elegante! Essa t-shirt essencial é feita de algodão de alta qualidade, oferecendo conforto duradouro e elegância versátil para qualquer ocasião.",
@@ -41,7 +41,7 @@ const data = [
   },
   {
     id: 5,
-    img: "../assets/img/camiseta_branca.svg",
+    img: "assets/img/camiseta_branca.svg",
     nameItem: "White T-Shirt",
     description:
       "Camiseta Básica: Simplicidade com Qualidade Premium! A camiseta básica, com tecido mais espesso, proporciona luxo e durabilidade, perfeita como base ou destaque no seu visual.",
@@ -51,7 +51,7 @@ const data = [
   },
   {
     id: 6,
-    img: "../assets/img/moletom.svg",
+    img: "assets/img/moletom.svg",
     nameItem: "Champion Jacket",
     description:
       "Jaqueta Embalável Champion: Estilo Prático para Qualquer Clima! A jaqueta embalável Champion, em poliéster de alta qualidade, une resistência a elementos com design moderno e praticidade de armazenamento.",
@@ -61,7 +61,7 @@ const data = [
   },
   {
     id: 7,
-    img: "../assets/img/boné.svg",
+    img: "assets/img/boné.svg",
     nameItem: "Snapback Hat",
     description:
       "Bem-Vindo ao Seu Novo Chapéu! O boné snapback é um clássico atemporal que combina com qualquer look, perfeito para o dia a dia ou para eventos especiais.",
@@ -71,7 +71,7 @@ const data = [
   },
   {
     id: 8,
-    img: "../assets/img/bermuda.svg",
+    img: "assets/img/bermuda.svg",
     nameItem: "Shorts Cargo",
     description:
       "Versátil e Confortável: Bermuda Cargo em Algodão! A bermuda cargo é um item essencial para o verão, combinando conforto e estilo para qualquer ocasião.",
@@ -81,7 +81,7 @@ const data = [
   },
   {
     id: 9,
-    img: "../assets/img/tenis.svg",
+    img: "assets/img/tenis.svg",
     nameItem: "Sports Shoes",
     description:
       "Tênis Esportivo: Conforto e Estilo para o Dia a Dia! O tênis esportivo é perfeito para atividades físicas ou para o uso diário, garantindo conforto e durabilidade.",
